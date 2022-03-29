3225101a2b88c3a1a3ae28b22b5cba9ff0bdc9d9
Jesus <jesisp17@gmail.com>
2022-03-29 13:56:40 -0300

    added events to the navbar and fixed typo on json

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
 export const navbar = `
   <nav class="navbar navbar-expand-lg navbar-light bg-secondary text-primary">
   <div class="container-fluid d-flex flex-row justify-content-evenly w-50">
-    <h3 class="primary pointer">Home</h3>
-    <h3 class="primary pointer">Menu</h3>
+    <h3 class="primary pointer" onclick="toggle()">Home</h3>
+    <h3 class="primary pointer" onclick="toggle()">Menu</h3>
   </div>
   </nav>
 `
