82266af226427f33ca1b4e2aa5eb9f6e101a5ad5
Jesus <jesisp17@gmail.com>
2022-03-29 13:54:13 -0300

    added json loader and json data

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,8 @@
 export const navbar = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary text-secondary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
-      </div>
-    </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-secondary text-primary">
+  <div class="container-fluid d-flex flex-row justify-content-evenly w-50">
+    <h3 class="primary pointer">Home</h3>
+    <h3 class="primary pointer">Menu</h3>
   </div>
-</nav>
+  </nav>
 `
